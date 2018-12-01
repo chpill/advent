dcc971caf2859309b9c198e641956c1c081571c7
chpill <etienne@spillemaeker.com>
2018-12-01 11:46:13 +0000

    advent 2018 - 1

---
+++ src/advent/eighteen/a1.clj
@@ -0,0 +1,33 @@
+(ns advent.seventeen.a12
+  (:require [clojure.java.io :as java-io]
+            [net.cgrand.xforms.io :as xforms-io]
+            [net.cgrand.xforms :as x]))
+
+
+
+(def input (->> (java-io/resource "eighteen/a1.txt")
+                java-io/file
+                xforms-io/lines-in
+                (into [] (map #(Integer. %)))))
+
+(reduce + input)
+;; => 576
+
+
+
+;;;;;;;;;;;;
+;; Part 2 ;;
+;;;;;;;;;;;;
+
+
+(loop [seen-frequencies #{}
+       current-frequency 0
+       input (cycle input)]
+  (if (get seen-frequencies current-frequency)
+    current-frequency
+    (recur (conj seen-frequencies current-frequency)
+           (+ current-frequency (first input))
+           (rest input))))
+
+;; 77674
+
